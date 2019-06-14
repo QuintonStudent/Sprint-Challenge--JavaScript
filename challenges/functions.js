cfd981b914a8557631f2769043c7acb94a4372bd
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-06-14 13:30:01 -0400

    callbacks portion of functions.js now complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,7 +6,7 @@
   * The last parameter accepts a callback
   * In the body of the function return the callback with the two parameters that you created
 */
-function myFunc(param1, param2, cb) {
+function consume(param1, param2, cb) {
   return cb(param1, param2);
 }
 
@@ -28,9 +28,9 @@ function greeting(firstName, lastName) {
 }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-consume(2,2,add); // 4
-consume(10,16,multiply); // 160
-consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ====
