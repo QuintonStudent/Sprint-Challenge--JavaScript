66e0e759c338a8d3f5b18bf5ea110035bca3644f
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-06-14 12:30:07 -0400

    completed arrays portion of objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -69,13 +69,17 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
 {"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
-{"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
+{"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}];
 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+graduates.forEach(item => {
+ universities.push(item.university);
+});
+universities.sort();
+console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student.
 
@@ -84,13 +88,28 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach(item => {
+  contactInfo.push(item.first_name + ' ' + item.email);
+});
+
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
-console.log(uni);
+// graduates.filter(whatever => {
+//   if ('Uni' === whatever.university[0] + whatever.university[1] + whatever.university[2]) {
+//     return uni.push(whatever.university);
+//   }
+// });
 
+for(let i = 0; i < graduates.length; i++){
+  if(graduates[i].university.includes('Uni')) {
+    uni.push(graduates[i].university);
+  }
+}
+
+console.log(uni);
 
 // ==== ADVANCED Array Methods ====
 
